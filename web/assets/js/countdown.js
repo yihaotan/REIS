65abac4d9492797cce86126f65842b50e64fa603
Laksa <boyangzheng.2012@sis.smu.edu.sg>
2015-03-03 23:22:42 +0800

    New UI First Draft
    New index

--- web/assets/js/countdown.js
+++ web/assets/js/countdown.js
@@ -1,23 +1,30 @@
+/**
+* Metis - Bootstrap-Admin-Template v2.3.2
+* Author : onokumus 
+* Copyright 2015
+* Licensed under MIT (https://github.com/onokumus/Bootstrap-Admin-Template/blob/master/LICENSE.md)
+*/
+
 /* Start Countdown Settings */
-
-var startDate = new Date("01/01/2013");
-var endDate = new Date("04/06/2014");
-
-var dif = endDate.getTime() - startDate.getTime();
-var difToSecond = dif / 1000;
-var defaultPercent = 0;
+/*global $:false */
+var startDate = new Date("01/01/2014"),
+    endDate = new Date("04/06/2015"),
+    dif = endDate.getTime() - startDate.getTime(),
+    difToSecond = dif / 1000,
+    defaultPercent = 0;
 
 
 $(function () {
     $('#counter').countdown({
-        until:endDate,
-        layout:'<div></div>',
-        onTick:updateBar
+        until: endDate,
+        layout: '<div></div>',
+        onTick: updateBar
     });
 
     $('a[rel=tooltip]').tooltip();
     $('div[rel=tooltip]').tooltip();
 });
+
 
 function updateBar(periods) {
 
@@ -28,28 +35,27 @@ function updateBar(periods) {
 
     fillTotalbar(periods[6] + periods[5] * 60 + periods[4] * 60 * 60 + periods[3] * 60 * 60 * 24);
 }
-;
 
 function fillSecondBar(percent) {
     $('#second-number').html(percent);
-    $('#second-bar').css('width', percent*100/60 + '%');
+    $('#second-bar').css('width', percent * 100 / 60 + '%');
 }
-;
+
 function fillMinuteBar(percent) {
     $('#minute-number').html(percent);
-    $('#minute-bar').css('width', percent*100/60 + '%');
+    $('#minute-bar').css('width', percent * 100 / 60 + '%');
 }
-;
+
 function fillHourBar(percent) {
     $('#hour-number').html(percent);
-    $('#hour-bar').css('width', percent*100/24 + '%');
+    $('#hour-bar').css('width', percent * 100 / 24 + '%');
 }
-;
+
 function fillDayBar(percent) {
     $('#day-number').html(percent);
     $('#day-bar').css('width', percent * 100 / 365 + '%');
 }
-;
+
 function fillTotalbar(percent) {
     defaultPercent = 100 - 100 * percent / difToSecond;
 
@@ -61,66 +67,62 @@ function fillTotalbar(percent) {
 
     $('#total-bar').css('width', defaultPercent + '%').html(currentPercent + '%');
 }
-;
+
 
 /* Start Google Map*/
 
 var map;
-function initialize() {
-    var mapOptions = {
-        zoom:7,
-        center:new google.maps.LatLng(40.711614, -73.995323),
-        mapTypeId:google.maps.MapTypeId.ROADMAP,
-        scrollwheel:false
-    };
-    map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
-}
 
-google.maps.event.addDomListener(window, 'load', initialize);
+map = new GMaps({
+        el: '#map_canvas',
+        lat: -12.043333,
+        lng: -77.028333
+    });
+
 
 /* Start Form validation*/
 
 $(function () {
 
     $('#emailForm').validate({
-        rules:{
-            email1:{
-                required:true,
-                email:true
+        rules: {
+            email1: {
+                required: true,
+                email: true
             }
         },
-        errorClass:'help-block',
-        errorElement:'span',
-        highlight:function (element, errorClass, validClass) {
-            $(element).parents('.control-group').removeClass('success').addClass('error');
+        errorClass: 'help-block',
+        errorElement: 'span',
+        highlight: function (element, errorClass, validClass) {
+            $(element).parents('.form-group').removeClass('has-success').addClass('has-error');
         },
-        unhighlight:function (element, errorClass, validClass) {
-            $(element).parents('.control-group').removeClass('error').addClass('success');
+        unhighlight: function (element, errorClass, validClass) {
+            $(element).parents('.form-group').removeClass('has-error').addClass('has-success');
         }
     });
     $('#messageForm').validate({
-        rules:{
-            name:{
-                required:true
+        rules: {
+            name: {
+                required: true
             },
-            email:{
-                required:true,
-                email:true
+            email: {
+                required: true,
+                email: true
             },
-            subject:{
-                required:true
+            subject: {
+                required: true
             },
-            message:{
-                required:true
+            message: {
+                required: true
             }
         },
-        errorClass:'help-inline',
-        errorElement:'span',
-        highlight:function (element, errorClass, validClass) {
-            $(element).parents('.control-group').removeClass('success').addClass('error');
+        errorClass: 'help-block',
+        errorElement: 'span',
+        highlight: function (element, errorClass, validClass) {
+            $(element).parents('.form-group').removeClass('has-success').addClass('has-error');
         },
-        unhighlight:function (element, errorClass, validClass) {
-            $(element).parents('.control-group').removeClass('error').addClass('success');
+        unhighlight: function (element, errorClass, validClass) {
+            $(element).parents('.form-group').removeClass('has-error').addClass('has-success');
         }
     });
 });
