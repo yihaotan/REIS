2760f35b4e6fdbbfd55691e7dca9ef1722ed1067
JovinHoon91 <jovin.hoon.2012@sis.smu.edu.sg>
2015-03-19 12:40:51 +0800

    changed connection manager

--- src/java/connection/ConnectionManager.java
+++ src/java/connection/ConnectionManager.java
@@ -22,8 +22,8 @@ public class ConnectionManager {
 
   static {
     // grab environment variable
-    //String host="127.9.152.130";//System.getenv("$OPENSHIFT_POSTGRESQL_DB_HOST");
-    String host = System.getenv("$OPENSHIFT_POSTGRESQL_DB_HOST");
+    String host="127.9.152.130";//System.getenv("$OPENSHIFT_POSTGRESQL_DB_HOST");
+    //String host = System.getenv("$OPENSHIFT_POSTGRESQL_DB_HOST");
     if (host != null) {
       // this is production environment
       // obtain database connection properties from environment variables
